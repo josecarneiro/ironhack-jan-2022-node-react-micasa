815170ec4de453837b8ecee65c6b75e974eef3bc
José Carneiro <contact@josecarnei.ro>
2022-06-21 20:31:46 +0100

    Complete suggestion feature (#1)

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -1,13 +1,13 @@
 import api from './api';
 
+export const signOutUser = () =>
+  api.post('/authentication/sign-out').then((response) => response.data);
+
 export const registerUser = (body) =>
   api.post('/authentication/sign-up', body).then((response) => response.data);
 
 export const logInUser = (body) =>
   api.post('/authentication/sign-in', body).then((response) => response.data);
-
-export const signOutUser = () =>
-  api.post('/authentication/sign-out').then((response) => response.data);
 
 export const loadUserInformation = () =>
   api.get('/authentication/me').then((response) => response.data);
